630eb9ed62f255cc05c8e5417b7e652d7deed957
Rashi Gupta <rashigupta9190@gmail.com>
2025-10-14 15:04:14 +0000

    no, I'm still getting the error Request failed Firebase: error (auth/api

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, browserSessionPersistence, setPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD-OOvczl91yzLGF80sAivuOuRgHvMvkLs",
+  authDomain: "ai-daily-planner-4a1f7.firebaseapp.com",
+  projectId: "ai-daily-planner-4a1f7",
+  storageBucket: "ai-daily-planner-4a1f7.appspot.com",
+  messagingSenderId: "765323928487",
+  appId: "1:765323928487:web:1d0d42b638af52e267cc82",
+  measurementId: "G-T905C97YCV"
 };
 
 // Initialize Firebase
@@ -36,8 +36,3 @@ setPersistence(auth, browserSessionPersistence)
 
 
 export { app, db, auth };
-
-
-
-
-
